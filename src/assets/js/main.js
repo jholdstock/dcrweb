adf8425696803eb71314732b3b242957756960ed
jholdstock <jholdstock@decred.org>
2024-01-15 09:57:46 +0000

    WIP 2

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,20 +4,38 @@ document.addEventListener("DOMContentLoaded", function() {
 	var os = platform.os.family;
 	
 	if (os == "Windows" || os == "Windows Server" || os == "Windows 7" || os == "Windows 7 / Server 2008 R2" || os == "Windows Server 2008 R2 / 7 x64") {
-		document.getElementById("windl").style.display = "block";
-		document.getElementById("alldl").style.display = "none";
+		elements = document.getElementsByClassName("windl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "block";
+		}
+		elements = document.getElementsByClassName("alldl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "none";
+		}
 	}
 
 	if (os == "CentOS" || os == "Debian" || os == "Fedora" || os == "Gentoo" || os == "Kubuntu" || os == "Linux Mint" || os == "Red Hat" || os == "SuSE" || os == "Ubuntu" || os == "Ubuntu Chromium" || os == "Xubuntu" || os == "Linux") {
-		document.getElementById("linuxdl").style.display = "block";
-		document.getElementById("alldl").style.display = "none";
+		elements = document.getElementsByClassName("linuxdl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "block";
+		}
+		elements = document.getElementsByClassName("alldl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "none";
+		}
 	}
 
 	if (os == "OS X") {
 		// If we detect OS X, we can't know if the user will want an amd or arm
 		// build. Just show the amd link which will work on both platforms.
-		document.getElementById("macdl").style.display = "block";
-		document.getElementById("alldl").style.display = "none";
+		elements = document.getElementsByClassName("macdl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "block";
+		}
+		elements = document.getElementsByClassName("alldl");
+		for (i = 0; i < elements.length; i++) {
+			elements[i].style.display = "none";
+		}
 	}
 });
 
